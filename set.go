3583721daf8708f3508499406c969b99d51135f7
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-08-25 07:06:03 -0700

    Add composite set types.

--- set.go
+++ set.go
@@ -7,6 +7,7 @@ package goline
  *  Description: 
  */
 import (
+    "strings"
     "fmt"
 )
 
@@ -14,6 +15,61 @@ import (
 type AnswerSet interface {
     Has(x interface{}) bool
     String() string
+}
+
+//  Composite answer sets (AnswerSetUnion and AnswerSetIntersection objects)
+//  are tools available to create more complex AnswerSet objects not provided
+//  by goline. For example open, or half-open, intervals can be constructed by
+//  taking the intersection of two bounded answer sets.
+type CompositeAnswerSet interface {
+    AnswerSet
+    Size() int
+    Set(i int) AnswerSet
+}
+
+func compositeString(composite CompositeAnswerSet, name string) string {
+    strs := make([]string, composite.Size())
+    for i := range strs {
+        strs[i] = composite.Set(i).String()
+    }
+    return fmt.Sprintf("%s of %s", strings.Join(strs, ", and "))
+}
+
+//  When making set intersections it is much easier to unknowingly create
+//  empty AnswerSets that cannot be detected effectively. Exercise extreme
+//  caution if creating set intersetions dynamically.
+type AnswerSetIntersection []AnswerSet
+type AnswerSetUnion []AnswerSet
+
+var (
+    Universe = AnswerSetIntersection{}
+    EmptySet = AnswerSetUnion{}
+)
+
+func (set AnswerSetIntersection) Size() int { return len(set) }
+func (set AnswerSetUnion) Size() int        { return len(set) }
+
+func (set AnswerSetIntersection) Set(i int) AnswerSet { return set[i] }
+func (set AnswerSetUnion) Set(i int) AnswerSet        { return set[i] }
+
+func (set AnswerSetIntersection) String() string { return compositeString(set, "intersection") }
+func (set AnswerSetUnion) String() string        { return compositeString(set, "union") }
+
+func (set AnswerSetIntersection) Has(x interface{}) bool {
+    for i := range set {
+        if !set[i].Has(x) {
+            return false
+        }
+    }
+    return true
+}
+func (set AnswerSetUnion) Has(x interface{}) bool {
+    for i := range set {
+        if set[i].Has(x) {
+            return true
+        }
+    }
+    return false
 }
 
 type Direction uint
@@ -122,36 +178,40 @@ func (set StringSet) String() string {
     return string(p)
 }
 
+//  An interval with only one bound, X.
 type UintBounded struct {
     Direction
     X   uint64
 }
+//  An interval with only one bound, X.
 type IntBounded struct {
     Direction
     X   int64
 }
+//  An interval with only one bound, X.
 type FloatBounded struct {
     Direction
     X   float64
 }
+//  An interval with only one bound, X.
 type StringBounded struct {
     Direction
     X   string
 }
 
-func (r UintBounded) String() string   {
+func (r UintBounded) String() string {
     if r.Direction == Above {
         return fmt.Sprintf("range [%v, %s)", r.X, r.Infinity())
     }
     return fmt.Sprintf("range (%s, %v]", r.Infinity(), r.X)
 }
-func (r IntBounded) String() string    {
+func (r IntBounded) String() string {
     if r.Direction == Above {
         return fmt.Sprintf("range [%v, %s)", r.X, r.Infinity())
     }
     return fmt.Sprintf("range (%s, %v]", r.Infinity(), r.X)
 }
-func (r FloatBounded) String() string  {
+func (r FloatBounded) String() string {
     if r.Direction == Above {
         return fmt.Sprintf("range [%v, %s)", r.X, r.Infinity())
     }
@@ -217,16 +277,22 @@ func (r StringBounded) Has(x interface{}) bool {
     panic(makeErrorMemberType(r, x))
 }
 
+//  In interval strictly bounded by a single number.
 type UintBoundedStrictly UintBounded
+//  In interval strictly bounded by a single number.
 type IntBoundedStrictly IntBounded
+//  In interval strictly bounded by a single number.
 type FloatBoundedStrictly FloatBounded
+//  In interval strictly bounded by a single number.
 type StringBoundedStrictly StringBounded
 
-// TODO Fix the String method so that it returns open intervals
-
+// TODO: Fix the String method so that it returns open intervals
 func (r UintBoundedStrictly) String() string   { return UintBounded(r).String() }
+// TODO: Fix the String method so that it returns open intervals
 func (r IntBoundedStrictly) String() string    { return IntBounded(r).String() }
+// TODO: Fix the String method so that it returns open intervals
 func (r FloatBoundedStrictly) String() string  { return FloatBounded(r).String() }
+// TODO: Fix the String method so that it returns open intervals
 func (r StringBoundedStrictly) String() string { return StringBounded(r).String() }
 
 func (r UintBoundedStrictly) Has(x interface{}) bool {
